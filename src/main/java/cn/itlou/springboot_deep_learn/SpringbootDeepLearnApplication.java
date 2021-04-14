85a083a78ee1ded6c70b059bda4aa384957393e7
java30kcoding <434356430@qq.com>
2021-04-14 20:41:06 +0800

    初始化SpringBoot2.4.4版本并集成默认依赖

---
+++ src/main/java/cn/itlou/springboot_deep_learn/SpringbootDeepLearnApplication.java
@@ -0,0 +1,13 @@
+package cn.itlou.springboot_deep_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeepLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeepLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/itlou/springboot_deep_learn/SpringbootDeepLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itlou.springboot_deep_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeepLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
